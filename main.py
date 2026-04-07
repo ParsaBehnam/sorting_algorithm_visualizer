545e91d3a2fab48d9c7daa77b9fd63b33606f882
ParsaBehnam <183300741+ParsaBehnam@users.noreply.github.com>
2026-04-07 18:13:40 +0300

    configure the user input setup and generation of the plot

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import random
+from visualizer import visualize_sort
+from bubble_sort import bubble_sort
+
 def main():
-    pass
+    size = int(input("Enter array size: "))
+
+    data = [random.randint(1, 100) for _ in range(size)]
+
+    generator = bubble_sort(data)
+
+    visualize_sort(generator, data, title="Bubble Sort")
 
 
 if __name__ == "__main__":
