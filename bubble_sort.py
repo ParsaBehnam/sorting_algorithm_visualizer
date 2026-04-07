66e947aba3de1bc8aa7680cf6ede6fa5944b1cfa
ParsaBehnam <183300741+ParsaBehnam@users.noreply.github.com>
2026-04-07 17:26:35 +0300

    define bubble_sort function

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,2 +1,22 @@
+from swap import swap
+
 def bubble_sort(arr):
-    pass
+    swapping = True
+    end = len(arr)
+
+    if len(arr) < 2:
+        yield arr.copy()
+        return
+    
+    while swapping:
+        swapping = False
+        for i in range(1, end):
+            yield arr.copy()
+            if arr[i - 1] > arr[i]:
+                swap(arr, i, i - 1)
+                swapping = True
+                yield arr.copy()
+        end -= 1
+
+    yield arr.copy()
+    
