4b0ed57e86aefa0b2f0f29e588447c55b106f725
ParsaBehnam <183300741+ParsaBehnam@users.noreply.github.com>
2026-04-07 17:42:32 +0300

    add canvas for the plot using fig and ax

--- visualizer.py
+++ visualizer.py
@@ -1,5 +1,14 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-def visualize_sort(generator, title='sorting_visualizer'):
-    pass
+def visualize_sort(generator, arr, title='sorting_visualizer'):
+    fig, ax = plt.subplots()
+    ax.set_title(title)
+    bar_rec = ax.bar(range(len(arr)), arr, align="edge")
+
+    ax.set_xlim(0, len(arr))
+    ax.set_ylim(0, max(arr) + 10)
+
+    text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
+    iteration = [0]
+    
